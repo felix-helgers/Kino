44dc105ae5a4ef92921def0ab40f42abb47f575e
Lunahrii <88711658+Lunahrii@users.noreply.github.com>
2023-05-21 19:44:01 +0200

    Plakate zur MainGUI hinzugefügt
    sponsored by ChatGPT

--- KinoJ/src/Default/MainGUI.java
+++ KinoJ/src/Default/MainGUI.java
@@ -1,22 +1,40 @@
 package Default;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+
+import Database.IDatabaseAdapter;
+import Database.SQLiteAdapter;
+
+import java.io.File;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.awt.GridBagConstraints;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
 
 public class MainGUI extends JFrame {
 	private static final long serialVersionUID = 1L;
 	JButton registerButton;
 	JButton signInButton;
-	JLabel userLabel;
+	private JLabel userLabel;
+	private JPanel filmplakatePanel;
+	private JScrollPane scrollPane;
+	private IDatabaseAdapter databaseAdapter;
+	private String  ordnerpfad = "D:\\Workspace-Eclipse\\Kino\\KinoJ\\src\\Bilder\\";
+
 	
 	public MainGUI() {
-		super("Main GUI");
+		super("Kino");
 		this.setSize(1500, 1000);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        databaseAdapter = SQLiteAdapter.getInstance();
         
         JPanel panel = new JPanel(new BorderLayout());
         JPanel topPanel = new JPanel(new BorderLayout());
@@ -25,6 +43,7 @@ public class MainGUI extends JFrame {
         userButtonPanel.add(new JLabel());
         
         userLabel = new JLabel("Username ");
+        userLabel.setVisible(false);
         userButtonPanel.add(userLabel);
         
         registerButton = new JButton("Registrieren");
@@ -35,8 +54,54 @@ public class MainGUI extends JFrame {
         
         topPanel.add(userButtonPanel, BorderLayout.EAST);
         panel.add(topPanel, BorderLayout.NORTH);
+        this.add(panel);
         
-        add(panel);
+        filmplakatePanel = new JPanel(new GridBagLayout());
+        scrollPane = new JScrollPane(filmplakatePanel);
+        panel.add(scrollPane, BorderLayout.CENTER);
+        this.setContentPane(panel);
+        
+      
+        
+        File ordner = new File(ordnerpfad);
+        File[] dateien = ordner.listFiles();
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10); // Abstand zwischen den Filmplakaten
+        
+        ArrayList<String> filme = this.databaseAdapter.getMoviesWithScreeningAndPoster();
+        
+        filme.forEach(film -> {
+        
+        ImageIcon bildIcon = new ImageIcon(ordnerpfad + film +".jpg");
+
+        JLabel bildLabel = new JLabel(bildIcon);
+        JLabel titelLabel = new JLabel(film);
+        JPanel plakatPanel = new JPanel(new BorderLayout());
+        plakatPanel.add(bildLabel, BorderLayout.CENTER);
+        plakatPanel.add(titelLabel, BorderLayout.SOUTH);
+        		
+        plakatPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //Placeholder
+                System.out.println("Plakat " + film + " wurde geklickt.");
+            }
+        });		
+        filmplakatePanel.add(plakatPanel, gbc);
+        gbc.gridx++;
+        
+        if (gbc.gridx % 3 == 0) {
+            gbc.gridx = 0;
+            gbc.gridy++;
+        		
+        }});
+        
+        
+        this.pack();
+        this.setLocationRelativeTo(null);
 	    setVisible(true);
 	}
 	
@@ -48,4 +113,6 @@ public class MainGUI extends JFrame {
     	signInButton.addActionListener(listener);
     	registerButton.addActionListener(listener);
     }
+    
+ 
 }
